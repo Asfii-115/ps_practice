c595b40ad549c90a77148c5d76bdf81f2a957ea4
Asfi <20101115@uap-bd.edu>
2024-09-24 20:37:47 +0600

    c14

--- codewars/c13.py
+++ codewars/c13.py
@@ -12,10 +12,19 @@
 # output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
 
 
-def open_or_senior(data):
-    return [
-        "Senior" if age >= 55 and handicap > 7 else "Open" for (age, handicap) in data
-    ]
+# def open_or_senior(data):
+#     return [
+#         "Senior" if age >= 55 and handicap > 7 else "Open" for (age, handicap) in data
+#     ]
 
 
-print(open_or_senior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]))
+# print(open_or_senior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]))
+
+
+def strin(s):
+    a = s.split()
+    a.sort()
+    return a
+
+
+print(strin("is2 Thi1s T4est 3a"))
---
+++ codewars/c14.py
@@ -0,0 +1,5 @@
+def shuffler(x):
+    return " ".join(x.split()[::-1])
+
+
+print(shuffler("asfi ahmed"))

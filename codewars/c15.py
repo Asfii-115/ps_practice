8e3f639c89d99493c654f9e7802fa175c02bc892
Asfi <20101115@uap-bd.edu>
2024-09-27 19:51:56 +0600

    c15

--- codewars/c15.py
+++ codewars/c15.py
@@ -1,5 +1,13 @@
-def merge_arrays(arr1, arr2):
-    return sorted(set(arr1 + arr2))
+# def merge_arrays(arr1, arr2):
+#     return sorted(set(arr1 + arr2))
 
 
-print(merge_arrays([1, 2, 3, 4, 5, 5], [6, 7, 8, 9, 10]))
+# print(merge_arrays([1, 2, 3, 4, 5, 5], [6, 7, 8, 9, 10]))
+
+
+def reverset(st):
+    # Your Code Here
+    return " ".join(st.strip().split()[::-1])
+
+
+print(reverset("Hello World"))

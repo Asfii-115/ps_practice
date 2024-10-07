e64262730a096eb12c1b7f8afca3772851b0eb4c
Asfi <20101115@uap-bd.edu>
2024-10-07 20:33:55 +0600

    diff

--- leetcode/leet24.py
+++ leetcode/leet24.py
@@ -5,3 +5,11 @@ def codds(low, high):
 
 
 print(codds(8, 10))
+
+
+def countOdds(low: int, high: int) -> int:
+    nums = (high - low) // 2
+    return nums if low % 2 == 1 or high % 2 == 1 else nums
+
+
+print(countOdds(3, 7))

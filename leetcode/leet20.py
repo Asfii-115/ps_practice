b0aadaf347cc8eb5d6bc65992c4a112cf52e94ea
Asfi <20101115@uap-bd.edu>
2024-10-08 19:18:26 +0600

    leet20

--- leetcode/leet20.py
+++ leetcode/leet20.py
@@ -1,12 +1,22 @@
-def plusone(digits):
-    n = len(digits)
-    if digits[-1] == 9:
-        if n == 1:
-            return [1, 0]
-        return digits[:-1] + [0]
-    else:
-        digits[-1] += 1
-    return digits
+# def plusone(digits):
+#     n = len(digits)
+#     if digits[-1] == 9:
+#         if n == 1:
+#             return [1, 0]
+#         return digits[:-1] + [0]
+#     else:
+#         digits[-1] += 1
+#     return digits
 
 
-print(plusone([1, 2, 9]))
+# print(plusone([1, 2, 9]))
+
+
+def containsDuplicate(nums):
+    for x in range(len(nums)):
+        if nums[x] in nums[x + 1 :]:
+            return True
+    return False
+
+
+print(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))

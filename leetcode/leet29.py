4f6394676f16000a2b525e01279f234f97c92f3b
Asfi <20101115@uap-bd.edu>
2024-10-13 19:56:39 +0600

    simu

--- leetcode/leet29.py
+++ leetcode/leet29.py
@@ -1,8 +1,23 @@
-def string_score(s):
-    count = 0
-    for x in range(1, len(s)):
-        count += abs(ord(s[x]) - ord(s[x - 1]))
-    return count
+# def string_score(s):
+#     count = 0
+#     for x in range(1, len(s)):
+#         count += abs(ord(s[x]) - ord(s[x - 1]))
+#     return count
 
 
-print(string_score("hello"))
+# print(string_score("hello"))
+
+
+def cal(operations):
+    li = []
+    for x in operations:
+        if x != "C" or x != "D":
+            li.append(int(x))
+        if x == "D":
+            li.append(x)
+        else:
+            li.append(x)
+    return li
+
+
+print(cal(["5", "2", "C", "D", "+"]))

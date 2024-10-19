c67da948ea00eefc8372e4890208ace98e6942cf
Asfi <20101115@uap-bd.edu>
2024-10-19 20:07:33 +0600

    merge

--- leetcode/leet30.py
+++ leetcode/leet30.py
@@ -10,12 +10,27 @@
 # print(remove_duplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
 
 
-def remove_element(nums, val):
-    count = 0
-    for x in range(len(nums)):
-        if nums[x] != val:
-            count += 1
-    return count
+# def remove_element(nums, val):
+#     count = 0
+#     for x in range(len(nums)):
+#         if nums[x] != val:
+#             count += 1
+#     return count
 
 
-print(remove_element([0, 1, 2, 2, 3, 0, 4, 2], 2))
+# print(remove_element([0, 1, 2, 2, 3, 0, 4, 2],2))
+
+
+def merge_sorted(nums1, nums2):
+    i = 0
+    j = 0
+    while i < len(nums1):
+        if nums1[i] <= nums2[j]:
+            i += 1
+        else:
+            nums1[i] = nums2[j]
+            j += 1
+    return nums1
+
+
+print(merge_sorted([1, 2, 3, 7, 8], [2, 5, 6]))

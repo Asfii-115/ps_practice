b02d33b7545efd4a7e3caa195f0aa74ff2206306
Asfi <20101115@uap-bd.edu>
2024-11-12 06:55:17 +0000

    c19

--- codewars/c19.py
+++ codewars/c19.py
@@ -46,3 +46,52 @@ def gimme(input_array):
     #a,b,c = sorted(input_array)
     return input_array.index(sorted(input_array)[1])
 print(gimme([2,3,1]))
+
+def sequence_sum(begin_number, end_number, step):
+    return sum(range(begin_number, end_number+1, step))
+
+print(sequence_sum(2,6,2))
+
+import math
+def round_to_next5(n):
+    mod = n%5
+    if mod==0: return n
+    else:
+        n+=5
+        k = n%5
+    return n - k    
+
+
+
+print(round_to_next5(7))
+
+
+def capitalize(s):
+    li = []
+    es = ''
+    os = ''
+    for x in range(len(s)):
+        if x%2==0:
+            es+=s[x].upper()
+        else:
+            es+=s[x]
+    for x in range(len(s)):
+        if x%2==1:
+            os+=s[x].upper()
+        else:
+            os+=s[x]        
+    li.append(es)       
+    li.append(os)       
+    return li    
+
+print(capitalize('abcde'))
+
+def sum_of_minimums(numbers):
+    i=0
+    for x in numbers:
+        m = min(sorted(x))
+        i+=m
+    return i    # use map()
+        
+
+print(sum_of_minimums([ [ 7,9,8,6,2 ], [6,3,5,4,3], [5,8,7,4,5] ]))

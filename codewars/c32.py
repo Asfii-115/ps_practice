6f8d9209b9dc7551e518fbeb82ae9e139e6f046c
Asfi <20101115@uap-bd.edu>
2024-11-22 20:39:06 +0600

    facto

--- codewars/c32.py
+++ codewars/c32.py
@@ -1,8 +1,15 @@
-s = input()
-s.lower()
-z_count = s.count("z")
-o_count = s.count("o")
-if 2 * z_count == o_count:
-    print("Yes")
-else:
-    print("No")
+# s = input()
+# s.lower()
+# z_count = s.count("z")
+# o_count = s.count("o")
+# if 2 * z_count == o_count:
+#     print("Yes")
+# else:
+#     print("No")
+
+
+n = int(input())
+ans = 1
+for x in range(1, n + 1):
+    ans *= x
+print(ans)
